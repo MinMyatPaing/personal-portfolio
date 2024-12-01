986195880d05573b48cbb5f2ed4b152b3935ed0e
MinMyatPaing <minmyatpaing64@gmail.com>
2024-12-01 10:33:00 +0000

    resources included

---
+++ src/assets/index.js
@@ -0,0 +1,60 @@
+import logo from "./logo.svg";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+import huddersfield from "./company/huddersfield.png";
+
+import grillsmith from "./projects/grillsmith.png";
+import minestate from "./projects/minestate.jpeg";
+import proshop from "./projects/proshop.png";
+import emotionscanner from "./projects/emotionscanner.jpeg";
+import petadopt from "./projects/petadopt.png";
+import plamories from "./projects/plamories.jpeg";
+
+export {
+  logo,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  huddersfield,
+  emotionscanner,
+  grillsmith,
+  minestate,
+  petadopt,
+  plamories,
+  proshop,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,196 @@
+import {
+  mobile,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  huddersfield,
+  grillsmith,
+  plamories,
+  petadopt,
+  minestate,
+  emotionscanner,
+  proshop,
+  threejs,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "projects",
+    title: "Projects",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Full Stack Web Developer",
+    icon: web,
+  },
+  {
+    title: "React Native Developer",
+    icon: mobile,
+  },
+  {
+    title: "ML Developer",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "AI Research Assistant",
+    company_name: "University of Huddersfield",
+    icon: huddersfield,
+    iconBg: "#383E56",
+    date: "July 2024 - Current",
+    points: [
+      "Assisting in the development and deployment of AI-driven applications, including integrating custom machine learning models and optimizing solutions for real-world user needs.",
+      "Designing and implemented a cloud-hosted dashboard to track user tasks programmatically, incorporating data visualization tools to enhance usability and team collaboration.",
+      "Conducting outreach and engagement activities, including promoting mental health research projects, refining app designs, and addressing user feedback to improve participation and user experience.",
+      "Ensuring project security and efficiency by implementing best practices for API key management, cost-effective AI model exploration, and robust app maintenance workflows.",
+    ],
+  },
+];
+
+const projects = [
+  {
+    name: "ProShop",
+    category: "web",
+    description:
+      "Web-based ecommerce platform that allows users to authenticate, purchase, and order online.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: proshop,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Job IT",
+    category: "web",
+    description:
+      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Trip Guide",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
